e0e5214f8d5670cb41c6d1c38e1168af2a63bc38
Adam McQuistan <sci.guy.mcq@gmail.com>
2022-03-30 13:25:05 -0500

    Initial commit

---
+++ test/aws-cdk-presigned-url-rest-api.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkPresignedUrlRestApi from '../lib/aws-cdk-presigned-url-rest-api-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-presigned-url-rest-api-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkPresignedUrlRestApi.AwsCdkPresignedUrlRestApiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
